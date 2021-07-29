dc025f55f72a74e14ffa7c21aed96dc1d869cd27
Diogo Albuquerque Dias de Souza <digsuk360@gmail.com>
2021-07-29 11:25:13 -0300

    Build with express server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Entrou");
+const express = require('express');
+const {resolve} = require('path');
+
+const app = express();
+
+app.use('/', express.static(
+    resolve(
+        __dirname,
+        './build'
+    )
+));
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if(err){
+        return console.log(err);
+    }
+    return console.log('Aplicação funcionando!')
+})
